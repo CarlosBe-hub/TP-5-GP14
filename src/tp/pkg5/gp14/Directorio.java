c9afe39c73f306a305e56f58e73cb852428d423a
Agustin-Benenatti <agus.benenatti09@gmail.com>
2024-09-03 19:34:11 -0300

    cambios
    Creacion de las clases Contacto y Directorio

---
+++ src/tp/pkg5/gp14/Directorio.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp.pkg5.gp14;
+
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.HashSet;
+
+/**
+ *
+ * @author aguse
+ */
+public class Directorio {
+    private  TreeMap<Long, Contacto> contactos;
+
+    public Directorio() {
+        contactos = new TreeMap<>();
+    }
+    
+    public void agregarContacto(Long telefono, Contacto contacto){
+        
+        contactos.put(telefono, contacto);
+        
+    }
+    
+    public Contacto buscarContacto(Long telefono){
+        
+        return contactos.get(telefono);
+        
+    }
+    
+    public Set<Long> buscarTelefono(String apellido){
+       Set<Long> numeros = new HashSet<>();
+        
+        for(Long telefono : contactos.keySet()){
+            if(contactos.get(telefono).getApellido().equalsIgnoreCase(apellido))
+                numeros.add(telefono);
+        }
+        return numeros;
+    }
+
+    
+    public ArrayList<Contacto> buscarContactos(String ciudad){
+        ArrayList<Contacto> listaContactos = new ArrayList<>();
+        
+        for(Contacto contacto : contactos.values()){
+            if(contacto.getCiudad().equalsIgnoreCase(ciudad))
+                listaContactos.add(contacto);
+        }
+        return listaContactos;
+    
+    }
+    
+    public void borrarContacto(Long telefono){
+        contactos.remove(telefono);
+    }
+}
