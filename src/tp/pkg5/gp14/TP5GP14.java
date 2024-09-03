6932017858d71ff6159a64fbd7e26dd0c7c935cf
CarlosBe-hub <carlosbenenatti21@gmail.com>
2024-09-03 18:55:19 -0300

    Initial commit

---
+++ src/tp/pkg5/gp14/TP5GP14.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp.pkg5.gp14;
+
+/**
+ *
+ * @author carlo
+ */
+public class TP5GP14 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
